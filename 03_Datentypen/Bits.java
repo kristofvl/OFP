f065ef0c3b5406a9a4e7aa40ef212b1080e330a1
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-27 07:07:51 +0200

    Create Bits.java

---
+++ 03_Datentypen/Bits.java
@@ -0,0 +1,13 @@
+/**
+  Binär zu Dezimal, https://docs.oracle.com/javase/8/docs/technotes/
+  guides/language/binary-literals.html
+  */
+
+class Bits {
+  public static void main( String[] str ) {
+    int y;  // Variable für Integer
+    y = 0b01010101;
+    System.out.print( "Binärzahl zu dezimal: " );
+    System.out.println( y );  // Methode zum Drucken auf die Konsole 
+  }
+}
