dd761aad634e2286f4fc206d5563009eecf43405
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-27 09:53:46 +0200

    Update Smiley.java

--- 03_Datentypen/Smiley.java
+++ 03_Datentypen/Smiley.java
@@ -4,14 +4,14 @@
   https://docs.oracle.com/javase/6/docs/api/java/lang/Character.html#unicode
   https://stackoverflow.com/questions/5903008/what-is-a-surrogate-pair-in-java
   */
-
 class Smiley {
   public static void main( String[] str ) {
     char c1 = 0x7117;  // Unicode Zeichen für: Soße aus Sojasoße, Mirin und Zucker
     System.out.println( c1 );
-
     char c2 = 0xD83D;  // Unicode Zeichen 'GRINNING FACE' in hexadezimal,
     char c3 = 0xDE00;  // als "surrogate pair"
     System.out.println( c2 + "" + c3 );  // für später: Wieso ""?
+    char c4 = 7;  // ASCII Zeichen für BEEP
+    System.out.println( c4 );
   }
 }
