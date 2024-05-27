dd53165724ba19aa1ad7226b412cd2d3489ff268
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-27 07:10:56 +0200

    Create CharASCII.java

---
+++ 03_Datentypen/CharASCII.java
@@ -0,0 +1,13 @@
+/**
+  ASCII-Tabelle: Kodierung für Zeichensätze
+  https://de.wikipedia.org/wiki/American_Standard_Code_for_Information_Interchange
+  */
+
+class CharASCII {
+  public static void main( String[] str ) {
+    char symbol;  // Variable für Zeichen
+    symbol = '@';
+    System.out.print( "Die ASCII-Kodierung für [" + symbol + "] ist " );
+    System.out.println( (int) symbol );  // Methode zum Drucken auf die Konsole 
+  }
+}
