7136aaf3c4683f6c76e237f5df2714e063945982
Kristof Van Laerhoven <kryztov@gmail.com>
2024-05-27 07:23:23 +0200

    Create Typkonversionen.java

---
+++ 03_Datentypen/Typkonversionen.java
@@ -0,0 +1,18 @@
+class Typkonversionen {
+  public static void main(String[] s) {
+    double x, y = 1.1;
+    int i, j = 5;
+    char c = 'a';
+    boolean b = true;
+    x = 3;         // OK: x == 3.0
+    x = 3/4;       // x == 0.0
+    x = c + y * j; // OK, Konversion nach double
+    i = j + x;     // Fehler: (j + x) hat Typ double
+    i = (int)x; 	  // OK, i = ganzzahl. Anteil von x
+    i = (int)b;    // Fehler: keine Konversion von boolean
+    b = i;         // Fehler: keine Konversion zu boolean
+    b = (i != 0);  // OK, Vergleich liefert boolean
+    c = c + 1;     // Fehler: c + 1 hat Typ int
+    c += 1;        // OK! c == ’b’
+  }
+}
