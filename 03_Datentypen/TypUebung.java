5a70af87529ee39eb2a9d60eef5de1b733cbc20d
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-02 09:04:34 +0200

    Create TypUebung.java

---
+++ 03_Datentypen/TypUebung.java
@@ -0,0 +1,20 @@
+class TypUebung {
+  public static void main( String[] s) {
+    /* Deklarieren und initialisieren Sie die folgenden Variablen und geben Sie sie in der Konsole aus: */
+
+    /* Variable zur Speicherung der Weltbevölkerung: */
+    
+    /* Variable zur Speicherung der Bevölkerungszahl eines Landes, gerundet in Millionen : */
+    
+    /* Variable zur Speicherung des prozentualen Anteils der Bevölkerung eines Landes an der Weltbevölkerung: */
+    
+    /* Variable, die speichert, ob die Bevölkerung eines Landes mehr als 1 % der Weltbevölkerung ausmacht: */
+    
+    /* Variablen zum Speichern dieses Symbols: ☺ */
+    
+    /* Konstante zum Speichern des Flaggensymbols eines Landes, z.B. 🇧🇪 : */
+
+    /* Konstante zum Speichern des Namens eines Landes, z.B. Belgien */
+
+  }
+}
