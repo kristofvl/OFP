0e7ac75bbe9e907ee2d562910b234844cf99ae5f
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-03 11:38:29 +0200

    Update Schleife01.java

--- 04_Anweisungen/Schleife01.java
+++ 04_Anweisungen/Schleife01.java
@@ -5,12 +5,6 @@
 
 class Schleife01 {
   public static void main( String[] str ) {
-   for (int i = 1; i <= 26; i++) {
-     if ( (i % 7) == 0 ) {
-       System.out.println("Hoppla");
-     } else {
-       System.out.println(i);
-     }
-   } // for
+    
   }
 }
