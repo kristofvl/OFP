074b0a4f5e4be4f2009278af357b3d42e2fd31ab
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-10 06:53:50 +0200

    Create WordleClass.java

---
+++ 06_ObjekteMethoden/WordleClass.java
@@ -0,0 +1,15 @@
+/**  WordleClass: Implementieren Sie das einfache Wordle-Spiel jetzt als Klasse:
+  */
+import java.util.Scanner;  // scan strings in der Konsole
+
+class WordleClass {
+  /*...*/   // Hier kommen die Attribute
+  
+  private String dialog( String hint ) { /*...*/ }  // private Methode 
+  public void run() { /*...*/ }  // public Methode 
+
+  public static void main( String[] str ) {  // main ist eine Klassenmethode
+    WordleClass w = new WordleClass();
+    w.run();
+  }
+}
