76f2a95c29e2381f2af21ca3f5445a809e9f476a
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-17 10:01:58 +0200

    Create StudentApp.java

---
+++ 06_ObjekteMethoden/StudentApp.java
@@ -0,0 +1,8 @@
+/** Erstellen Sie eine Klasse StudentApp, die über die Methode main() ein Objekt
+    der Klasse Student erzeugt. Die Klasse Student sollte einen Namen für den
+    Schüler, eine Matrikelnummer und eine Note als private Attribute enthalten.
+    Der Konstruktor von Student sollte es erlauben, den Namen und die
+    Matrikelnummer anzugeben. Zusätzliche öffentliche Methoden von Student sollten
+    das Setzen und Abrufen der Note ermöglichen.
+    */
+
