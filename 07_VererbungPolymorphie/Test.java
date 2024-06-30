35a0e031d2145d76d7556fb234f72d2624b050e1
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-30 18:59:17 +0200

    Add files via upload

---
+++ 07_VererbungPolymorphie/Test.java
@@ -0,0 +1,13 @@
+/** Implementieren Sie eine Klasse namens Test, die in main() ein
+    Objekt k der Klasse Kreis mit dem Radius 2 erzeugt und dann die
+    Oberfläche mit der Methode getFlaeche von k ausgibt. */
+
+
+/** Implementieren Sie eine Schnittstelle Figur, die ein Attribut
+    namens PI mit dem Wert 3.1415 und eine Methode namens
+    getFlaeche() hat, die eine Variable mit Typ double zurückgibt. */
+
+
+/** Implementieren Sie eine Unterklasse von Figur, genannt Kreis,
+    die ein Attribut Radius und die Methode getFlaeche hat. */
+
