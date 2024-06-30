532fd0c1808a939272f7d0e0f40320c7695d80ed
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-30 14:49:56 +0200

    Create TextLogger.java

---
+++ 09_Serialisierung/TextLogger.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class TextLogger {
+  public static void main(String[] args) {
+    // IOException soll in main gefangen werden:
+
+      FileOutputStream out = new FileOutputStream("log.txt");  // Ausgabe
+      // Zeichen einlesen bis Stromende (ˆD), schreibe jedes Zeichen
+      // via out in die Ausgabedatei:
+
+      // Datei schließen:
+    
+  }
+}
