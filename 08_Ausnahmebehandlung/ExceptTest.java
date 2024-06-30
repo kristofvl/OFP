195990ba6bc2159a812d2647e1f1dfd8c2499219
Kristof Van Laerhoven <kryztov@gmail.com>
2024-06-30 16:39:58 +0200

    Create ExceptTest.java

---
+++ 08_Ausnahmebehandlung/ExceptTest.java
@@ -0,0 +1,7 @@
+class ExceptTest {
+  public static void main(String[] args) {
+    int a = (int)( 10*Math.random() );
+    int b = (int)( 10*Math.random() );
+    System.out.println("a/b=" + (a/b));
+  }
+}
