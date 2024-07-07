b66439c5449016937477cb16ed125e5b2dde4dcd
Kristof Van Laerhoven <kryztov@gmail.com>
2024-07-07 19:52:56 +0200

    Create Lookup.java

---
+++ 10_ThreadsSockets/Lookup.java
@@ -0,0 +1,18 @@
+import java.net.*;
+public class Lookup {
+  public static void main(String[] args) {
+    if (args.length != 1) {
+     System.err.println("Usage: java Listing <host>");
+     System.exit(1);
+    }
+    try {  // Get requested address
+      InetAddress addr = InetAddress.getByName(args[0]);
+      System.out.println(addr.getHostName());
+      System.out.println(addr.getHostAddress());
+    }
+    catch (UnknownHostException e) {
+      System.err.println(e.toString());
+      System.exit(1);
+    }
+  }
+}
