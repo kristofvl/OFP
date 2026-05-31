796af53efc16f8f3d8ce2a1145b6922d8a660e53
Kristof Van Laerhoven <kvl@eti.uni-siegen.de>
2026-05-31 11:51:12 +0200

    update

---
+++ 03_Datentypen/GleitkommaZahlen.java
@@ -0,0 +1,13 @@
+/**
+   Schreibe ein Java-Programm, das folgende Informationen speichert:
+   1. die genaue Breitengrad-Position eines GPS-Signals z.B. 52.520008123
+   2. der Preis eines Supermarktartikels z.B. 32.10 oder 7.55
+   3. die Entfernung eines Raumfahrtobjekts zur Erde in Metern z.B. 225 Milliarden
+   4. die °C Temperatur eines Schlafzimmers z.B. 21.5 oder 32
+  */
+
+class GleitkommaZahlen {
+  public static void main( String[] str ) {
+
+  }
+}
