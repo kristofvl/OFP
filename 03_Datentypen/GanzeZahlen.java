45602cba1be2ebee3f7b14834c32d13d82254a1b
Kristof Van Laerhoven <kvl@eti.uni-siegen.de>
2026-05-31 10:10:49 +0200

    update

---
+++ 03_Datentypen/GanzeZahlen.java
@@ -0,0 +1,14 @@
+/**
+   Schreibe ein Java-Programm, das folgende Informationen speichert:
+   1. Die Einwohnerzahl einer kleinen Stadt z.B. 85.000
+   2. Die Tag-zu-Tag Temperaturdifferenz z.B. +8 oder -17
+   3. Die Anzahl der Schüler an einer Schule z.B. 1200
+   4. Die Weltbevölkerung z.B. 8.000.000.000
+  */
+
+class GanzeZahlen {
+  public static void main( String[] str ) {
+
+
+  }
+}
